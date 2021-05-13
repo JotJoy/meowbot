afd0ef8d9e2a0666f080626d5fada9199d25a6fb
Nyyaann <69776412+Nyyaan@users.noreply.github.com>
2021-05-12 22:10:31 -0700

    Create random-avatar.js

---
+++ commands/random-avatar.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'random-avatar',
+    aliases: ['ra'],
+    permissions: [],
+    cooldown: 5,
+    description: 'Will grab random users icons that have this bot added to the server ',
+    execute(message, args, cmd, client, Discord){
+
+        const user = client.users.cache.random();
+
+        const embed = new Discord.MessageEmbed()
+            embed.setTitle(`${user.tag}'s Avatar`)
+            embed.setImage(user.displayAvatarURL({dynamic: true, format: "png", size: 4096}))
+            embed.setColor("#ffc9f8")
+            embed.setFooter("ID: " + user.id)
+            .setTimestamp()
+        return message.channel.send(embed)
+        
+
+    }
+};
