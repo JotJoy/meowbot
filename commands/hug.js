b91a84c69a546f6197d14b6c9c6de43e23c240f4
JotJoy <70596821+JotJoy@users.noreply.github.com>
2021-05-13 18:53:57 +0200

    hug.js

---
+++ commands/hug.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const nekoclient = require('nekos.life');
+const neko = new nekoclient();
+
+module.exports = {
+    name: 'hug',
+    permissions: [],
+    description: "Hug someone!",
+    execute (message, args, cmd, client, Discord) {
+        
+        if (!message.guild) return;
+            if (message.mentions.members.size === 0) {
+            async function no_ping() {
+                const GIF = await neko.sfw.hug();
+                const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('##ffc9f8')
+                .setTitle(`${message.author.tag} hugged themselves`)
+                .setImage(GIF.url)
+                message.channel.send(exampleEmbed); 
+            }
+            no_ping();
+            }
+            if (message.mentions.members.size !== 0) {
+            async function ping() {
+            const user = message.mentions.users.first();
+            const GIF = await neko.sfw.hug();
+            const exampleEmbed = new Discord.MessageEmbed()
+            .setColor('##ffc9f8')
+            .setTitle(`${message.author.tag} hugged ${user.tag}`)
+            .setImage(GIF.url)
+            message.channel.send(exampleEmbed); 
+            }
+            ping();
+        }
+    }
+}
