853440f5c3d060e32f4a220cc9628e25cb26667a
Nyyaann <69776412+Nyyaan@users.noreply.github.com>
2021-05-12 21:46:50 -0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -7,17 +7,19 @@ module.exports = {
     execute(message, args, cmd, client, Discord){
 
         const embed = new Discord.MessageEmbed()
+        let mentionedMember = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+        if (!mentionedMember) mentionedMember = message.member;
         if(!message.mentions.users.first()){
             embed.setTitle("Your Avatar:")
             embed.setDescription("This is your avatar.")
-            embed.setImage(message.author.displayAvatarURL({dynamic: true, format: "png", size: 4096}))
+            embed.setImage(mentionedMember.user.displayAvatarURL({dynamic: true, format: "png", size: 4096}))
             embed.setColor("#ffc9f8")
             return message.channel.send(embed)
         }else{
-            const user = message.mentions.users.first() || client.users.cache.get(u => u.id === args[0])
-            embed.setTitle(`${user.tag}'s Avatar:`)
+            const user = message.mentions.users.first()
+            embed.setTitle(mentionedMember.user.tag + "'s Avatar:")
             embed.setDescription(`This is ${user.tag}'s avatar.`)
-            embed.setImage(user.displayAvatarURL({dynamic: true, format: "png", size: 4096}))
+            embed.setImage(mentionedMember.user.displayAvatarURL({dynamic: true, format: "png", size: 4096}))
             embed.setColor('#ffc9f8')
             embed.setFooter("ID: " + user.id)
             .setTimestamp()
